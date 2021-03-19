0dfcded655ead87e6fef69de12524e8a9d00e70e
Literal Line <>
2021-03-18 21:02:18 -0400

    update link

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@
   var headerHTML =
     '<hr>' +
     '<p>by Literal Line&emsp;' +
-    'Source code: <a href="https://github.com/quique-gq/games" target="_blank">Github</a>&emsp;' +
+    'Source code: <a href="https://github.com/quique-gq/g" target="_blank">Github</a>&emsp;' +
     'more at <a href="https://quique.gq" target="_blank">Quique.gq</a>' +
     '</p>';
   document.getElementsByTagName('footer')[0].insertAdjacentHTML('afterbegin', headerHTML);
