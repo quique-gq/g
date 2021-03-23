eef74c31a26f11ed0d5d9767bca3a5c082a9b22b
Literal Line <13988479+literal-line@users.noreply.github.com>
2021-03-23 10:18:30 -0400

    Update site.js

--- site.js
+++ site.js
@@ -1,7 +1,7 @@
 // bad site code i think
 
 (function () {
-  var CURRENT_VERSION = 'mar232021-1013est';
+  var CURRENT_VERSION = 'mar232021-1017est';
 
   var cookieCheck = function (callback) {
     var c = JSON.parse(localStorage.getItem('gameData')) || {};
