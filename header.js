f73805dc678d9a53aca2d180887f19fdf030978d
Literal Line <chickenfishbird2@gmail.com>
2021-03-23 13:20:04 +0000

    improve 404 page, add margin to stuff

--- header.js
+++ header.js
@@ -1,6 +1,6 @@
 var HEADER = (function () {
   var headerHTML =
-    '<a href="/"><img width="300px" height="130px" src="./assets/logo_g.svg"></a>' +
+    '<a href="/"><img width="277px" height="120px" src="./assets/logo_g.svg"></a>' +
     '<nav>' +
     '<ul>' +
     '<li><a href="/featured">Featured</a></li>' +
