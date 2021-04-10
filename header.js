6625f60505819560bb142865cd64c719ab13069d
Literal Line <>
2021-04-10 16:38:00 -0400

    remove beta text

--- header.js
+++ header.js
@@ -1,6 +1,6 @@
 var HEADER = (function () {
   var headerHTML =
-    '<a href="/" class="svgOutline" style="text-decoration: none"><img id="logo" src="./assets/logo_g.svg"><span style="font-weight: bold; font-style: italic; color: #304888; text-shadow: -1px 1px 0 #000, 1px 1px 0 #000, 1px -1px 0 #000, -1px -1px 0 #000; letter-spacing: 3px">Beta</span></a>' +
+    '<a href="/" class="svgOutline" style="text-decoration: none"><img id="logo" src="./assets/logo_g.svg"></a>' +
     '<nav>' +
     '<ul class="ilList">' +
     '<li><a class="hover au" href="/featured">Featured</a></li>' +
